3a3a9e60773c72b4a46c2f2a449cba69b05f5472
rahmat058 <md.kazi.rahmat@gmail.com>
2018-12-26 00:23:04 +0600

    footer and scroll spy is working done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import 'materialize-css/dist/css/materialize.min.css'
 import './assets/css/main.css'
+import M from 'materialize-css'
+
 
 import Navbar from './components/navbar'
 import Slider from './components/slider'
@@ -10,8 +12,15 @@ import PopularPlaces from './components/popular-places'
 import Follow from './components/follow'
 import Gallery from './components/gallery'
 import Contact from './components/contact'
+import Footer from './components/footer'
 
 class App extends Component {
+  componentDidMount() {
+    //Scroll Spy
+    let ss = document.querySelectorAll(".scrollspy");
+    M.ScrollSpy.init(ss, {})
+  }
+
   render() {
     return (
       <div id='home' className='scrollspy'>
@@ -23,6 +32,7 @@ class App extends Component {
         <Follow />
         <Gallery />
         <Contact />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="section teal darken-2 white-text center">
+        <p className="flow-text">TravelVille &copy; 2018</p>
+      </footer>
+    )
+  }
+}
+
+export default Footer
